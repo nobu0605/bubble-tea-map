426cb520a229995c0986e20d5b831fcfd363511e
鈴木　孝伸 <>
2020-05-05 19:25:17 +0900

    added three shops data

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,48 +5,93 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow,
 } from "react-google-maps";
 import chatime from "../shopData/chatime.json";
+import GongCha from "../shopData/GongCha.json";
+import ChunShuiTang from "../shopData/ChunShuiTang.json";
 import { apiKey } from "../config";
+import iconsMarker from "../images/iconsMarker.png";
+import icons30 from "../images/icons30.png";
 
 type Props = any;
+type State = any;
 
-export default class MyMapComponent extends React.Component<Props> {
+const shops = [
+  { shopObj: chatime, icon: iconsMarker },
+  { shopObj: GongCha, icon: null },
+  { shopObj: ChunShuiTang, icon: icons30 },
+];
+
+export default class MyMapComponent extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: 0,
+      selectedPlace: {},
+    };
   }
 
-  componentDidMount() {}
+  onMarkerClick(props: any, selected: number, e: any) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: selected,
+      showingInfoWindow: true,
+    });
+  }
 
   render() {
     const MyMapComponent: any = compose(
       withProps({
         googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${apiKey}`,
         loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `800px` }} />,
+        containerElement: <div style={{ height: `1200px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
       }),
       withScriptjs,
       withGoogleMap
     )((props: any) => (
-      <GoogleMap
-        defaultZoom={12}
-        defaultCenter={{ lat: 35.6970463, lng: 139.7004277 }}
-      >
-        {props.isMarkerShown &&
-          chatime.map((location: any) => {
-            return (
-              <div>
-                <Marker
-                  position={{ lat: location.lat, lng: location.lng }}
-                  onClick={props.onMarkerClick}
-                />
-              </div>
-            );
-          })}
-      </GoogleMap>
+      <div>
+        <GoogleMap
+          defaultZoom={5}
+          defaultCenter={{ lat: 35.6970463, lng: 139.7004277 }}
+        >
+          {this.props.isMarkerShown &&
+            shops.map((shop: any) => {
+              return shop.shopObj.map((shopData: any, index: number) => {
+                return (
+                  <div>
+                    <Marker
+                      position={{ lat: shopData.lat, lng: shopData.lng }}
+                      onClick={(e) => this.onMarkerClick(this.props, index, e)}
+                      icon={shop.icon}
+                    >
+                      {index === this.state.activeMarker &&
+                        this.state.showingInfoWindow && (
+                          <InfoWindow>
+                            <div>
+                              <b>
+                                <span>{shopData.shop_name}</span>
+                              </b>
+                              <br />
+                              <span>{shopData.address}</span>
+                            </div>
+                          </InfoWindow>
+                        )}
+                    </Marker>
+                  </div>
+                );
+              });
+            })}
+        </GoogleMap>
+      </div>
     ));
 
-    return <MyMapComponent isMarkerShown={this.props.isMarkerShown} />;
+    return (
+      <div>
+        <MyMapComponent />
+      </div>
+    );
   }
 }

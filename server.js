dd85aa78f25741529d3fcc4cfabe2cd793e0ee13
鈴木　孝伸 <>
2020-05-07 00:42:48 +0900

    Initial commit

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-var app = express();
-const dotenv = require("dotenv");
-dotenv.config();
-app.use(express.static(__dirname + "/"));
-app.listen(process.env.PORT || 8080);
